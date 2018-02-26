ad2505ce70137e3d26fdd88102f69605eb2fcbbb
porkaipandian <35136321+porkaipandian@users.noreply.github.com>
2018-02-26 16:01:21 +0530

    Create count36.c

---
+++ count36.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	char a[50];
+	int i,count=0;
+	scanf("%s",&a[i]);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if((a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='Z')||(a[i]>='0'&&a[i]<='9'));
+		else
+		
+count++;
+		
+		
+	}
+		printf("%d",count);
+
+		return 0;
+}
+
+
